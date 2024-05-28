9355122f207ac5d70c6a1823a9e1d23e24d67b60
Fredy <inztockz@gmail.com>
2024-05-28 17:21:12 -0500

    Se agrego el controller de Docente (falta agregar el eliminar)

---
+++ Proyecto_Escolar/src/main/java/com/escolar/controller/DocenteController.java
@@ -0,0 +1,56 @@
+package com.escolar.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.escolar.entitys.Docente;
+import com.escolar.services.DocenteService;
+import com.escolar.services.DocenteServicesImpl;
+
+@RestController
+@RequestMapping("/api/docente")
+public class DocenteController {
+	
+	@Autowired
+	private DocenteService docSer;
+	
+	@GetMapping("")
+	public List<Docente> listarDocentes(){
+		return docSer.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Docente> listarDocentePorId(int id){
+		Optional<Docente> docente = docSer.findById(id);
+		return docente.map(value -> ResponseEntity.ok().body(value)).orElse(null);
+	}
+	
+	@PostMapping("/save")
+	public ResponseEntity<Docente> registrarDocente(@RequestBody Docente docente){
+		Docente guardarDocente = docSer.save(docente);
+		return ResponseEntity.status(HttpStatus.CREATED).body(guardarDocente);
+	}
+	
+	@PutMapping
+	public ResponseEntity<Docente> actualizarDocente(@PathVariable int id, @RequestBody Docente docente){
+		Docente actualizarDocente = docSer.update(id, docente);
+		if(actualizarDocente != null) {
+			return ResponseEntity.ok().body(actualizarDocente);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+}
